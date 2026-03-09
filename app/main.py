f00887e6c83a00a6ec3ed1a3bcb4e93ab207aac8
Skraldz <mathaeo@gmail.com>
2026-03-09 11:28:57 +0100

    Added POST /movies endpoint with TMDB integration

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@
 from fastapi import FastAPI # Imports the fastAPI functions
 from database import SessionLocal # Imports the session that contains the database engine
 from models import Movie, Actor, Genre # Imports the models to work in the database
-from tmdb import search_movies # Imports the search movies function from tmdb.py
+from tmdb import search_movies, get_movie_details, get_movie_credits # Imports the search movies function from tmdb.py
+from datetime import date # Imports the datetime library
 app = FastAPI() # The chosen API for this projects backend
 
 @app.get("/")       # When someone makes a GET-request to /, run this function
@@ -19,3 +20,24 @@ def get_movies():
 @app.get("/search")     # Queries the server to search TMDB for an input
 def search(query: str): # Defines the search function and a string query
     return search_movies(query) # Returns the response.json from the search_movies(query) in tmdb.py
+
+@app.post("/movies")
+def add_movie(tmdb_id: int):
+    movie_credits = get_movie_credits(tmdb_id)
+    movie_details = get_movie_details(tmdb_id)
+    new_movie = Movie(
+        title = movie_details["title"],
+        how_long = movie_details["runtime"],
+        added_by = "default_user", # will be updated once user-system has been implemented
+        tmdb_id = movie_details["id"],
+        date_added = date.today(),
+        seen = False, # Update this to be an option in the future, so you can add a film you already have seen, but want the other user to see
+        released = int(movie_details["release_date"][:4]),
+        country = None, # kunne ikke finde korrekte detalje
+        languages = movie_details["original_language"]
+    )
+    db = SessionLocal()
+    db.add(new_movie)
+    db.commit()
+    db.close()
+    return "Movie added succesfully!"
--- app/tmdb.py
+++ app/tmdb.py
@@ -9,7 +9,21 @@ token = os.getenv("TMDB_API_TOKEN") # Sets token variable to be the TMDB API tok
 def search_movies(query):   # Function that searches TMDB for a match with users search input
     headers = {"Authorization": f"Bearer {token}"}  # adds the TMDB API token in the header of the GET to TMDB's API
     response = httpx.get("https://api.themoviedb.org/3/search/movie", # Sets the response variable as the results from the GET request to TMDBs API
-       headers=headers, # Includes the API token header in the GET request
-       params={"query": query} # Sets the search parameters to be the user input on the client side
-    ) 
+      headers=headers, # Includes the API token header in the GET request
+      params={"query": query} # Sets the search parameters to be the user input on the client side
+   ) 
     return response.json() # Returns the TMDB search query to the user
+
+def get_movie_details(tmdb_id): # Function that gets details on a movie based on TMDB data
+   headers = {"Authorization": f"Bearer {token}"} # adds the TMDB API token in the header of the GET to TMDB's API
+   response = httpx.get(f"https://api.themoviedb.org/3/movie/{tmdb_id}", # Sets the response variable as the results from the GET request to TMDBs API
+      headers=headers, # Includes the API token header in the GET request
+   )
+   return response.json() # Returns the TMDB movie details in .json
+
+def get_movie_credits(tmdb_id): # Function that gets movie credits based on TMDB data
+   headers = {"Authorization": f"Bearer {token}"} # Adds the TMDB API token in the header
+   response = httpx.get(f"https://api.themoviedb.org/3/movie/{tmdb_id}/credits", # Sets the response variable as the results from the GET request to TMDBs API
+      headers=headers, # Includes the API token header in the GET request
+   )
+   return response.json() # Returns the TMDB credits in .json
