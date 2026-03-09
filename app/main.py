fa0e33b4f691de047b2b6bf8ea81ef84e90642d2
Skraldz <mathaeo@gmail.com>
2026-03-09 11:35:57 +0100

    Updated comments

--- app/main.py
+++ app/main.py
@@ -21,20 +21,20 @@ def get_movies():
 def search(query: str): # Defines the search function and a string query
     return search_movies(query) # Returns the response.json from the search_movies(query) in tmdb.py
 
-@app.post("/movies")
-def add_movie(tmdb_id: int):
-    movie_credits = get_movie_credits(tmdb_id)
-    movie_details = get_movie_details(tmdb_id)
-    new_movie = Movie(
-        title = movie_details["title"],
-        how_long = movie_details["runtime"],
+@app.post("/movies")    # Adds a movie to the database via http POST
+def add_movie(tmdb_id: int): # Which executes the add_movies function with the tmdb_id
+    movie_credits = get_movie_credits(tmdb_id) # gets movie credits from the tmdb_id
+    movie_details = get_movie_details(tmdb_id) # gets movie details from the tmdb_id
+    new_movie = Movie( #adds the new movie as a movie to the database
+        title = movie_details["title"], # sets title as title from TMDB details
+        how_long = movie_details["runtime"], # sets how_long as runtime from TMDB details
         added_by = "default_user", # will be updated once user-system has been implemented
-        tmdb_id = movie_details["id"],
-        date_added = date.today(),
+        tmdb_id = movie_details["id"], # sets tmdb_id as the TMBD id
+        date_added = date.today(), # sets date_added as the date
         seen = False, # Update this to be an option in the future, so you can add a film you already have seen, but want the other user to see
-        released = int(movie_details["release_date"][:4]),
-        country = None, # kunne ikke finde korrekte detalje
-        languages = movie_details["original_language"]
+        released = int(movie_details["release_date"][:4]), # takes the 4 first integers from tmdbs release date, which corresponds to the release year (YYYY-MM-DD)
+        country = None, # Will be set at a later date
+        languages = movie_details["original_language"] # sets languages as original_language from TMDB details, should be updated to be its own table at some point because of dubs, multilang etc.
     )
     db = SessionLocal()
     db.add(new_movie)
