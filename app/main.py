456168340b4ff050d6810cfd894f41463e4f8bdf
Skraldz <mathaeo@gmail.com>
2026-03-10 10:27:39 +0100

    Implemented genre and actor saving with junction tables

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 # Library imports
 from fastapi import FastAPI # Imports the fastAPI functions
 from database import SessionLocal # Imports the session that contains the database engine
-from models import Movie, Actor, Genre # Imports the models to work in the database
+from models import Movie, Actor, Genre, Movie_genres, Movie_actors # Imports the models to work in the database
 from tmdb import search_movies, get_movie_details, get_movie_credits # Imports the search movies function from tmdb.py
 from datetime import date # Imports the datetime library
 app = FastAPI() # The chosen API for this projects backend
@@ -23,7 +23,7 @@ def search(query: str): # Defines the search function and a string query
 
 @app.post("/movies")    # Adds a movie to the database via http POST
 def add_movie(tmdb_id: int): # Which executes the add_movies function with the tmdb_id
-    movie_credits = get_movie_credits(tmdb_id) # gets movie credits from the tmdb_id
+    movie_credits = get_movie_credits(tmdb_id) # TODO: implement actor saving to movie_actors table
     movie_details = get_movie_details(tmdb_id) # gets movie details from the tmdb_id
     new_movie = Movie( #adds the new movie as a movie to the database
         title = movie_details["title"], # sets title as title from TMDB details
@@ -36,8 +36,50 @@ def add_movie(tmdb_id: int): # Which executes the add_movies function with the t
         country = None, # Will be set at a later date
         languages = movie_details["original_language"] # sets languages as original_language from TMDB details, should be updated to be its own table at some point because of dubs, multilang etc.
     )
-    db = SessionLocal()
-    db.add(new_movie)
-    db.commit()
-    db.close()
-    return "Movie added succesfully!"
+    db = SessionLocal() # Sets database as the local session
+    db.add(new_movie) # adds (new_movie) to the database
+    db.commit() # commits the changes to the database
+    db.refresh(new_movie) # Refreshes the movie variable so it contains the unique database ID it gets when commited
+
+    for genre in movie_details["genres"]: # For-loop for genre in movie_details (TMDB's genre)
+        existing_genre = db.query(Genre).filter(Genre.genre == genre["name"]).first() # existing_genre = query the database model [Genre] with a filter that searches the [Genre] table to see if there already exists a genre in the database with the same name as the TMDB genre name
+        if not existing_genre: # If there isn't a matching genre in the [Genre] table
+            new_genre = Genre(genre=genre["name"]) # Set new_genre = TMDB genre name, as a genre object in the [Genre] table
+            db.add(new_genre) # Add the genre to the database
+            db.commit() # Commit the changes
+            db.refresh(new_genre) # Refreshes the genre variable so it contains the unique database ID it gets when commited
+        
+        genre_to_link = existing_genre if existing_genre else new_genre # makes the genre_to_link variable into the genre variable, if it's an existing or new one
+        movie_genre_link = Movie_genres(
+        movie_id = new_movie.id,
+        genre_id = genre_to_link.id
+        )
+        db.add(movie_genre_link)
+        db.commit()
+
+    for cast in movie_credits["cast"]:
+        existing_actor = db.query(Actor).filter(Actor.actor_name == cast["name"]).first()
+        if not existing_actor:
+            new_actor = Actor(actor_name=cast["name"])
+            db.add(new_actor)
+            db.commit()
+            db.refresh(new_actor)
+
+        actor_to_link = existing_actor if existing_actor else new_actor
+        movie_actor_link = Movie_actors(
+            movie_id = new_movie.id,
+            actor_id = actor_to_link.id
+        )
+        db.add(movie_actor_link)
+        db.commit()
+    db.close() # closes the database
+    return "Movie added succesfully!"  
+
+ 
+    
+    
+    
+    
+
+
+ 
--- app/models.py
+++ app/models.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, Integer, String, Date, Boolean
+# Library imports - imports functions from sqlalchemy
+from sqlalchemy import Column, Integer, String, Date, Boolean, ForeignKey
 from sqlalchemy.orm import DeclarativeBase
 
-class Base(DeclarativeBase):
-    pass
+class Base(DeclarativeBase): # The main interaction between our database and python - Declarative base is the way we add entries to each table
+    pass  # Passes on arguments
 
-
-class Movie(Base):
-    __tablename__ = "movies"
+class Movie(Base): # Creats a class for the table movies in the database
+    __tablename__ = "movies" # Assigns the class to the table name in the database
     id = Column(Integer, primary_key=True)
     title = Column(String(255))
     how_long = Column(Integer)
@@ -27,3 +27,13 @@ class Genre(Base):
     __tablename__ = "genres"
     id = Column(Integer, primary_key=True)
     genre = Column(String(255))
+
+class Movie_genres(Base):   # Creates a class for the table movie_genres in the database
+    __tablename__ = "movie_genres" # Assigns the class to the table name
+    movie_id = Column(Integer, ForeignKey("movies.id"), primary_key=True) # sets a column to contain the variable movie_id as an integer with the id value of the movies in the movies table, and sets it as a primary key in this table
+    genre_id = Column(Integer, ForeignKey("genres.id"), primary_key=True) # sets a column to contain the variable genre_id as an integer with the id value of the genre in the genres table, and sets it as a primary key in this table
+
+class Movie_actors(Base):
+    __tablename__ = "movie_actors"
+    movie_id = Column(Integer, ForeignKey("movies.id"), primary_key=True)
+    actor_id = Column(Integer, ForeignKey("actors.id"), primary_key=True)
