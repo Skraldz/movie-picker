c7d23d16a65b722ae97e5f34c684829f20aafc89
Skraldz <mathaeo@gmail.com>
2026-03-27 11:12:10 +0100

    Add Dockerfile and backed service to docker-compose

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ load_dotenv() # Starts up dotenv, so the password can be picked from the .env fi
 
 password = os.getenv("MOVIE_PICKER_DB_PASSWORD") # Sets password to be the mysqlroot password collected from the .env file
 
-DATABASE_URL = f"mysql+pymysql://movie_picker_user:{password}@192.168.5.10:3306/movie_db" # Where on which machine my database is located
+DATABASE_URL = f"mysql+pymysql://movie_picker_user:{password}@movie_picker_db:3306/movie_db" # Where on which machine my database is located
 
 engine = create_engine(DATABASE_URL) # Creates the engine for my database, so it can interact with main.py
 
