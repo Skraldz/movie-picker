78ae5f9ab28f1e69cd5476ce2412e5662c53808a
Skraldz <mathaeo@gmail.com>
2026-04-14 13:38:56 +0200

    Added button interactivity in AddMovie.jsx

--- frontend/src/AddMovie.jsx
+++ frontend/src/AddMovie.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 function AddMovie() {
     const [title, setTitle] = useState('')
     const [query, setQuery] = useState(null)
+    const [addedMovies, setAddedMovies] = useState([])
 
         function searchMovie(){
             const url = new URL('http://192.168.5.10:8000/search/')
@@ -11,6 +12,20 @@ function AddMovie() {
                 .then(res => res.json())
                 .then(data => setQuery(data))
         }
+
+        function addMovie(id){
+            const url = new URL(`http://192.168.5.10:8000/movies?tmdb_id=${id}`)
+            fetch(url, {
+                method: 'POST'
+            })
+        }
+
+    useEffect(() => {
+        fetch('http://192.168.5.10:8000/movies')
+            .then(res => res.json())
+            .then(data => setAddedMovies(data))
+    }, [])
+
     return(
         <div>
             <h2>Add a Movie to the Database</h2>
@@ -19,15 +34,20 @@ function AddMovie() {
 
             {query && (
                 <div>
-                    {query.results.map(movie => (
-                        <div key={movie.id}>
-                            <p>{movie.title}, {movie.release_date.slice(0, 4)}</p>
-                            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-                            <p>{movie.overview}</p>
-                            <button>Add Movie</button>
-                            <button>View on TMDB</button>
-                        </div>
-                    ))}
+                    {query.results.map(movie => {
+                        const isAdded = addedMovies.some(dbMovie => dbMovie.tmdb_id === movie.id.toString())
+                        return (
+                            <div key={movie.id}>
+                                <p>{movie.title}, {movie.release_date.slice(0, 4)}</p>
+                                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+                                <p>{movie.overview}</p>
+                                <button disabled={isAdded} onClick={() => addMovie(movie.id)}>
+                                    {isAdded ? 'Already Added' : 'Add Movie'}
+                                </button>
+                                <button>View on TMDB</button>
+                            </div>
+                        )
+                    })}
                 </div>
             )}
         </div>
